5d2659adc5984537737d9125f2a290753c233982
Nils-Martin Robeling <33074402+Ponnis@users.noreply.github.com>
2019-12-04 15:40:54 +0100

    Update BasicGFSurfer.java

--- eclipse/Robots/src/BasicGFSurfer.java
+++ eclipse/Robots/src/BasicGFSurfer.java
@@ -18,8 +18,12 @@ import java.awt.Color;
  */
 
 public class BasicGFSurfer extends AdvancedRobot {
+	
+	//#if VisitCountStats
     public static int BINS = 47;
     public static double _surfStats[] = new double[BINS]; // we'll use 47 bins
+    //#endif
+    
     //#if WaveSurfing
     public Point2D.Double _myLocation;     // our bot's location
     public Point2D.Double _enemyLocation;  // enemy bot's location
@@ -28,11 +32,14 @@ public class BasicGFSurfer extends AdvancedRobot {
     public ArrayList _surfDirections;
     public ArrayList _surfAbsBearings;
     //#endif
+    
     //#if GuessFactorTargeting
 	private static final double BULLET_POWER = 1.9;
 	//#endif
+	
 	private static double lateralDirection;
 	private static double lastEnemyVelocity;
+	
 	//#if OrbitalMovement && RandomMovement
 	private static GFTMovement movement;
 	 
@@ -58,14 +65,16 @@ public class BasicGFSurfer extends AdvancedRobot {
     public static double WALL_STICK = 160;
 
     public void run() {
+    	//#if ChangeRobotColour
 		setColors(Color.BLUE, Color.BLACK, Color.YELLOW);
+		//#endif
 		lateralDirection = 1;
 		lastEnemyVelocity = 0;
 		
         _enemyWaves = new ArrayList();
         _surfDirections = new ArrayList();
         _surfAbsBearings = new ArrayList();
-
+        //#if SpinningRadar
         setAdjustGunForRobotTurn(true);
         setAdjustRadarForGunTurn(true);
 
@@ -74,16 +83,17 @@ public class BasicGFSurfer extends AdvancedRobot {
             turnRadarRightRadians(Double.POSITIVE_INFINITY);
         } while (true);
     }
-
+    //#endif
+    
     public void onScannedRobot(ScannedRobotEvent e) {
     	//#if WaveSurfing
         _myLocation = new Point2D.Double(getX(), getY());
 
         double lateralVelocity = getVelocity()*Math.sin(e.getBearingRadians());
         double absBearing = e.getBearingRadians() + getHeadingRadians();
-
+        
         setTurnRadarRightRadians(Utils.normalRelativeAngle(absBearing - getRadarHeadingRadians()) * 2);
-
+        
         _surfDirections.add(0,
             new Integer((lateralVelocity >= 0) ? 1 : -1));
         _surfAbsBearings.add(0, new Double(absBearing + Math.PI));
@@ -135,7 +145,7 @@ public class BasicGFSurfer extends AdvancedRobot {
 		//#if OrbitalMovement && RandomMovement
 		movement.onScannedRobot(e);
 		//#endif
-		//#ifGuessFactorTargeting
+		//#if GuessFactorTargeting && InfinityLock
 		setTurnRadarRightRadians(Utils.normalRelativeAngle(enemyAbsoluteBearing - getRadarHeadingRadians()) * 2);
 		//#endif
     }
@@ -368,6 +378,7 @@ public class BasicGFSurfer extends AdvancedRobot {
     //#endif
     // got this from RaikoMicro, by Jamougha, but I think it's used by many authors
     //  - returns the absolute angle (in radians) from source to target points
+  //#if WaveSurfing
     public static double absoluteBearing(Point2D.Double source, Point2D.Double target) {
         return Math.atan2(target.x - source.x, target.y - source.y);
     }
@@ -375,11 +386,12 @@ public class BasicGFSurfer extends AdvancedRobot {
     public static double limit(double min, double value, double max) {
         return Math.max(min, Math.min(value, max));
     }
-
+    
     public static double bulletVelocity(double power) {
         return (20D - (3D*power));
     }
-
+    
+    
     public static double maxEscapeAngle(double velocity) {
         return Math.asin(8.0/velocity);
     }
@@ -403,6 +415,7 @@ public class BasicGFSurfer extends AdvancedRobot {
             robot.setAhead(100);
         }
     }
+    //#endif
 }
 //#if GuessFactorTargeting
 class GFTWave extends Condition {
@@ -477,6 +490,7 @@ class GFTWave extends Condition {
 }
 
 //#endif
+//#if GuessFactorTargeting || (OrbitalMovement && RandomMovement)
 class GFTUtils {
 	static double bulletVelocity(double power) {
 		return 20 - 3 * power;
@@ -499,6 +513,8 @@ class GFTUtils {
 		return Math.max(min, Math.min(max, v));
 	}
 }
+//#endif
+
 //#if OrbitalMovement && RandomMovement
 class GFTMovement {
 	private static final double BATTLE_FIELD_WIDTH = 800;
