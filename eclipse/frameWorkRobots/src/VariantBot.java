0daac9e98ae6487fd9c3c085275ed2355c729b6d
ChalmersBock <erikboc@student.chalmers.se>
2019-12-10 16:55:16 +0100

    Created variantbot

---
+++ eclipse/frameWorkRobots/src/VariantBot.java
@@ -0,0 +1,17 @@
+import gun.AbstractGun;
+import robocode.AdvancedRobot;
+import robocode.ScannedRobotEvent;
+
+public class VariantBot extends AdvancedRobot {
+
+    AbstractGun gun;
+
+    public void run(){
+
+    }
+
+    public void onScannedRobot(ScannedRobotEvent e){
+        
+    }
+
+}
