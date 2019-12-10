41fc6998475d8777ecdfa6e012edffae8e928859
Stefan Chan <stefan2846@hotmail.com>
2019-12-10 17:27:04 +0100

    Added gun package

---
+++ eclipse/frameWorkRobots/src/gun/AbstractGun.java
@@ -0,0 +1,5 @@
+package gun;
+
+public abstract class AbstractGun {
+	abstract Double getFiringAngle();
+}
---
+++ eclipse/frameWorkRobots/src/gun/LinearTargetingGun.java
@@ -0,0 +1,13 @@
+package gun;
+
+import robocode.util.*;
+import java.awt.geom.*;
+
+public class LinearTargetingGun extends AbstractGun {
+	
+	@Override
+	public Double getFiringAngle() {
+		
+	}
+	
+}
